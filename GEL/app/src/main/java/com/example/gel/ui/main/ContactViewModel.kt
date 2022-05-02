f419e92f73c96a5fc25138b6e47c02aed16584f2
YCL-01 <youngchanlim@naver.com>
2022-05-02 13:37:46 +0900

    fragment

---
+++ GEL/app/src/main/java/com/example/gel/ui/main/ContactViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.gel.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class ContactViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- GEL/app/src/main/java/com/example/gel/ui/main/SectionsPagerAdapter.kt
+++ GEL/app/src/main/java/com/example/gel/ui/main/SectionsPagerAdapter.kt
@@ -8,7 +8,8 @@ import com.example.gel.R
 
 private val TAB_TITLES = arrayOf(
     R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_text_2,
+    R.string.tab_text_3
 )
 
 class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
@@ -16,11 +17,12 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
 
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
-        if(position === 0){
-            return MedicalFragment.newInstance()
-        }else{
-            return SecurityFragment.newInstance()
+        when(position){
+            0 -> return MedicalFragment.newInstance()
+            1 -> return SecurityFragment.newInstance()
+            2 -> return ContactFragment.newInstance()
         }
+        return MedicalFragment.newInstance()
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
@@ -29,6 +31,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
 
     override fun getCount(): Int {
         // Show 2 total pages.
-        return 2
+        return 3
     }
 }
