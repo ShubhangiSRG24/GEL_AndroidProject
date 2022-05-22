280eebef08fc4a615cdea45b7c83d04fde6248af
YCL01 <youngchanlim@naver.com>
2022-05-23 04:26:47 +0900

    design update

--- GEL_beta/app/src/main/java/com/example/gel_beta/ui/user_information/UserFragment.kt
+++ GEL_beta/app/src/main/java/com/example/gel_beta/ui/user_information/UserFragment.kt
@@ -10,7 +10,7 @@ import com.example.gel_beta.R
 
 class UserFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false);
+        val view = inflater.inflate(R.layout.fragment_user, container, false);
 
         return view
     }
