c8cf77df4425a5d9fb9e6dd6274856a6ea8e5d98
ShubhangiSRG24 <ShubhangiSaile.Garnaik@stonybrook.edu>
2022-05-22 15:50:54 +0900

    contacts init

--- GEL_beta/app/src/main/java/com/example/gel_beta/ui/contacts/ContatctsFragment.kt
+++ GEL_beta/app/src/main/java/com/example/gel_beta/ui/contacts/ContatctsFragment.kt
@@ -15,9 +15,6 @@ class ContatctsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:Bundle?): View? {
 
-
-
-
         return inflater.inflate(R.layout.fragment_contacts, container, false);
     }
 
